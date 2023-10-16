ec3addc47c4c9d35bd05e7a7a888c7bdb4068c98
wemik2323 <wemik232@yandex.ru>
2023-10-17 01:41:17 +0300

    Java lab1 is not done yet...

--- Java/lab1/lab1.java
+++ Java/lab1/lab1.java
@@ -1,21 +1,26 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 class lab1 {
     public static void main(String args[]) {
         double[] array1 = new double[6];
         double[] array2 = new double[7];
-        System.out.println("A faggot");
+        ArrayList<Double> arr1 = new ArrayList<Double>(6);
+        LinkedList<Double> arr2 = new LinkedList<Double>();
         Scanner systemIn = new Scanner(System.in);
-        System.out.println(solution(systemIn, array1));
-        System.out.println(solution(systemIn, array2));
-        
+  //      System.out.println(task1(systemIn, array1));
+  //      System.out.println(task1(systemIn, array2));
+    
+        System.out.println(task2(systemIn, arr1.toArray()));
+        System.out.println(task2(systemIn, arr2.toArray()));
 
         array1_out(array1, array2);
     }
 
-    static double solution(Scanner systemIn, double[] array) {
+    static double task1(Scanner systemIn, double[] array) {
         for(int i = 0; i < Array.getLength(array); ++i) {
             array[i] = Math.sqrt(systemIn.nextInt());
         }
@@ -23,6 +28,18 @@ class lab1 {
         double arrayMin = array[0];
         for (var i : array)
             arrayMin = Math.min(arrayMin, i);
+
+        return arrayMin;
+    }
+
+   
+    static Object task2(Scanner systemIn, Object[] array) {
+        for(int i = 0; i < Array.getLength(array); ++i) {
+            array[i] = Math.sqrt(systemIn.nextInt());
+        }
+        System.out.println(array);
+
+        Object arrayMin = array[0];
 
         return arrayMin;
     }

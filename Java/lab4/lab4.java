ec4c515ff075c371cff6343310a4a4ec49d6fa32
wemik2323 <wemik232@yandex.ru>
2023-11-23 07:54:53 +0300

    It's getting a little bit of risky here...

--- Java/lab4/lab4.java
+++ Java/lab4/lab4.java
@@ -1,6 +1,7 @@
 package Java.lab4;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,66 +16,94 @@ import java.util.Map;
 //word, frequency, frequency%, mostFrequentWord, MostFrequentWordAmount, MostRareWord, MostRareWordAmount
 
 public class lab4 {
+    public static HashMap<Character, Float> mapChars;
+    public static HashMap<String, Float> mapWords;
+    public static float amountOfChars;
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
             System.out.println("Введите минимум один аргумент!");
             return;
         }
-        if (args.length > 0) {
-
+        if (args.length > 0 && args[0].indexOf(".txt") != -1) {
+            float amountOfWords = 0;
+            lab4.mapWords = new HashMap<String, Float>();
+            lab4.mapChars = new HashMap<Character, Float>();
+            amountOfWords = makeHashMaps(args[0], (int)amountOfWords);
+            LinkedHashMap<String, Float> sortedMap = makeSortedMap();
             for (String filename: args) {
                 if (filename.indexOf(".txt") == -1) {
-                    if (args[1].indexOf(".csv") != -1) {
-                        float amountOfWords = 0;
-                        HashMap<String, Float> map = makeHashMap(args[0], (int)amountOfWords);
-                        amountOfWords = map.get("AOW");
-                        map.remove("AOW");
-                        LinkedHashMap<String, Float> sortedMap = makeSortedMap(map);
-                        float percentage = makeCsvFile(sortedMap, (int)amountOfWords, filename);
-                        System.out.println(percentage);
+                    if (filename.indexOf(".csv") != -1) {
+                        makeCsvFile(sortedMap, (int)amountOfWords, filename);
                     }
-                    if (args[1].indexOf(".json")!= -1) {
-                        makeJsonFile();
+                    if (filename.indexOf(".json")!= -1) {
+                        makeJsonFile(sortedMap, (int)amountOfWords, filename);
                     }
                 }
             }
+        } else if (args[0] == "-h" || args[0] == "--help") {
+            System.out.println("Для выполнения программы выполните следующие условия:");
+            System.out.println("1. Программа должна считывать текстовый файл первым аргументом.");
+            System.out.println("2. Последующие аргументы должны быть в формате \"CSV\" или \"JSON\".");
+            System.out.println("3. Можно указывать несколько файлов вывода в разном порядке.");
+            System.out.println("4. Можно использовать флаг \"-r\" или \"remove\" для того чтобы удалить все созданные файлы.");
+            return;
+        } else if (args[0] == "-r" || args[0] == "--remove") {
+            File file = new File("");
+            File folder = new File();
+            File fList[] = folder.listFiles();
+            // Searchs .lck
+            for (int i = 0; i < fList.length; i++) {
+                String pes = fList.get(i);
+                if (pes.contains(".lck") == true) {
+                    // and deletes
+                    boolean success = (new File(fList.get(i)).delete());
+                }
+            }
+        } else {
+            System.out.println("Передайте файл формата \"TXT\" для работы программы.");
+            return;
         }
-
+        System.out.println("Работа программы успешно завершена.");
         return;
     }
 
-    static HashMap<String, Float> makeHashMap(String pathTextFile, int amountOfWords) throws IOException {
+    static float makeHashMaps(String pathTextFile, int amountOfWords) throws IOException {
         StringBuilder word = new StringBuilder();
-        HashMap<String, Float> map = new HashMap<String, Float>();
 
         String text = new String ((Files.readAllBytes(Paths.get(pathTextFile))), "UTF-8");
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) > 47 && text.charAt(i) < 58 || text.charAt(i) > 64 && text.charAt(i) < 91 || text.charAt(i) > 96 && text.charAt(i) < 123 || 
-            text.charAt(i) > 1039 && text.charAt(i) < 1104) {
+            lab4.amountOfChars++;
+            // if (text.charAt(i) > 47 && text.charAt(i) < 58 || text.charAt(i) > 64 && text.charAt(i) < 91 || text.charAt(i) > 96 && text.charAt(i) < 123 || 
+            // text.charAt(i) > 1039 && text.charAt(i) < 1104) {
+            if (Character.isLetterOrDigit(text.charAt(i))) {
+                if (lab4.mapChars.containsKey(text.charAt(i))) {
+                    lab4.mapChars.put(text.charAt(i), lab4.mapChars.get(text.charAt(i)) + 1);
+                } else {
+                    lab4.mapChars.put(text.charAt(i), 1f);
+                }
                 word.append(text.charAt(i));
                 if (i == text.length() - 1) {
-                    if (map.containsKey(word.toString())) {
-                    map.put(word.toString(), map.get(word.toString()) + 1);
+                    if (lab4.mapWords.containsKey(word.toString())) {
+                    lab4.mapWords.put(word.toString(), lab4.mapWords.get(word.toString()) + 1);
                     } else {
-                        map.put(word.toString(), 1f);
+                        lab4.mapWords.put(word.toString(), 1f);
                     }
                     amountOfWords++;
                 }
             } else if (word.length() > 0) {
-                if (map.containsKey(word.toString())) {
-                    map.put(word.toString(), map.get(word.toString()) + 1);
+                if (lab4.mapWords.containsKey(word.toString())) {
+                    lab4.mapWords.put(word.toString(), lab4.mapWords.get(word.toString()) + 1);
                 } else {
-                    map.put(word.toString(), 1f);
+                    lab4.mapWords.put(word.toString(), 1f);
                 }
                 word = new StringBuilder();
                 amountOfWords++;
             }
         }
-        map.put("AOW", (float)amountOfWords);
-        return map;
+        return amountOfWords;
     }
 
-    static String getMinKey(HashMap<String, Float> map) {
+    static String getMinKeyWord(HashMap<String, Float> map) {
         String minKey = null;
         for (java.util.Map.Entry<String, Float> entryLocal : map.entrySet()) {
             if (Collections.min(map.values()) == entryLocal.getValue()) {
@@ -83,7 +112,7 @@ public class lab4 {
         }
         return minKey;
     }
-    static String getMaxKey(HashMap<String, Float> map) {
+    static String getMaxKeyWord(HashMap<String, Float> map) {
         String maxKey = null;
         for (java.util.Map.Entry<String, Float> entryLocal : map.entrySet()) {
             if (Collections.max(map.values()) == entryLocal.getValue()) {
@@ -92,16 +121,34 @@ public class lab4 {
         }
         return maxKey;
     }
+    static Character getMinKeyChar(HashMap<Character, Float> map) {
+        Character minKey = null;
+        for (java.util.Map.Entry<Character, Float> entryLocal : map.entrySet()) {
+            if (Collections.min(map.values()) == entryLocal.getValue()) {
+                minKey = entryLocal.getKey();
+            }
+        }
+        return minKey;
+    }
+    static Character getMaxKeyChar(HashMap<Character, Float> map) {
+        Character maxKey = null;
+        for (java.util.Map.Entry<Character, Float> entryLocal : map.entrySet()) {
+            if (Collections.max(map.values()) == entryLocal.getValue()) {
+                maxKey = entryLocal.getKey();
+            }
+        }
+        return maxKey;
+    }
 
-    static LinkedHashMap<String, Float> makeSortedMap(HashMap<String, Float> map) {
+    static LinkedHashMap<String, Float> makeSortedMap() {
         ArrayList<Float> list = new ArrayList<>();
         LinkedHashMap<String, Float> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<String, Float> entry : map.entrySet()) {
+        for (Map.Entry<String, Float> entry : lab4.mapWords.entrySet()) {
             list.add(entry.getValue());
         }
         Collections.sort(list);
         for (int i = list.size()-1; i > 0; i--) {
-            for (java.util.Map.Entry<String, Float> entry : map.entrySet()) {
+            for (java.util.Map.Entry<String, Float> entry : lab4.mapWords.entrySet()) {
                 if (entry.getValue().equals(list.get(i))) {
                     sortedMap.put(entry.getKey(), list.get(i));
                 }
@@ -110,17 +157,14 @@ public class lab4 {
         return sortedMap;
     }
 
-    static float makeCsvFile(LinkedHashMap<String, Float> sortedMap, int amountOfWords, String File) throws IOException {
-        String pathCSVFILE = File;
-        BufferedWriter csvWriter = new BufferedWriter(new FileWriter(pathCSVFILE));
+    static void makeCsvFile(LinkedHashMap<String, Float> sortedMap, int amountOfWords, String File) throws IOException {
+        BufferedWriter csvWriter = new BufferedWriter(new FileWriter(File));
         csvWriter.write("word;frequency;frequency%;mostFrequentWord;MostFrequentWordAmount;MostRareWord;MostRareWordAmount;\n");
         boolean flag = true;
-        float percentage = 0;
         for (String key : sortedMap.keySet()) {
-            percentage += sortedMap.get(key) * 100/amountOfWords;
             if (flag) {
-                csvWriter.write(key + ";" + sortedMap.get(key) + ";" + sortedMap.get(key) * 100 / amountOfWords + ";" + getMaxKey(sortedMap) + ";" +
-                Collections.max(sortedMap.values()) + ";" + getMinKey(sortedMap) + ";" + Collections.min(sortedMap.values()) + "\n");
+                csvWriter.write(key + ";" + sortedMap.get(key) + ";" + sortedMap.get(key) * 100 / amountOfWords + ";" + getMaxKeyWord(sortedMap) + ";" +
+                Collections.max(sortedMap.values()) + ";" + getMinKeyWord(sortedMap) + ";" + Collections.min(sortedMap.values()) + "\n");
 
                 flag = false;
                 continue;
@@ -128,10 +172,36 @@ public class lab4 {
             csvWriter.write(key + ";" + sortedMap.get(key) + ";" + sortedMap.get(key) * 100 / amountOfWords + "\n");
         }
         csvWriter.close();
-        return percentage;
+        return;
     }
 
-    static void makeJsonFile() {
+    static void makeJsonFile(LinkedHashMap<String, Float> sortedMap, int amountOfWords, String filename) throws IOException {
+        BufferedWriter jsonWriter = new BufferedWriter(new FileWriter(filename));
+        String tab = "    ";
+        jsonWriter.write("{\n");
+        jsonWriter.write(tab + "\"name\": " + "\"" + filename + "\",\n");
+        jsonWriter.write(tab + "\"amountOfChars\": " + "\"" + lab4.amountOfChars + "\",\n");
+        jsonWriter.write(tab + "\"mostRepeatedChar\": " + "{\n");
+        jsonWriter.write(tab + tab + "\"char\": " + "\"" + getMaxKeyChar(mapChars) + "\",\n");
+        jsonWriter.write(tab + tab + "\"amount\": " + Collections.max(mapChars.values()) + "\n");
+        jsonWriter.write(tab + tab + "},\n");
+        jsonWriter.write(tab + "\"rarestChar\": " + "{\n");
+        jsonWriter.write(tab + tab + "\"char\": " + "\"" + getMinKeyChar(mapChars) + "\",\n");
+        jsonWriter.write(tab + tab + "\"amount\": " + Collections.min(mapChars.values()) + "\n");
+        jsonWriter.write(tab + tab + "},\n");
+        jsonWriter.write(tab + "\"mostFrequentWord\": " + "{\n");
+        jsonWriter.write(tab + tab + "\"word\": " + "\"" + getMaxKeyWord(sortedMap) + "\",\n");
+        jsonWriter.write(tab + tab + "\"amount\": " + Collections.max(sortedMap.values()) + "\n");
+        jsonWriter.write(tab + tab + "},\n");
+        jsonWriter.write(tab + "\"rarestWord\": " + "{\n");
+        jsonWriter.write(tab + tab + "\"word\": " + "\"" + getMinKeyWord(sortedMap) + "\",\n");
+        jsonWriter.write(tab + tab + "\"amount\": " + Collections.min(sortedMap.values()) + "\n");
+        jsonWriter.write(tab + tab + "}\n");
 
+
+
+        jsonWriter.write("}");
+        jsonWriter.close();
+        return;
     }
 }

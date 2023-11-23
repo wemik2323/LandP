681ff8412a98b12809babbb981f9dba3a7b2c7a6
wemik2323 <wemik232@yandex.ru>
2023-11-23 04:26:53 +0300

    New DagaevLabs

--- DagaevLabs/lab2/script.ps1
+++ DagaevLabs/lab2/script.ps1
@@ -1,49 +1,32 @@
-# Check if the first argument is provided
-if (-not $args[0]) {
-    Write-Output "Using: $PSCommandPath"
-    Write-Output "Enter word"
+if ($args.Length -eq 0) {
+    Write-Host "Использование: $MyInvocation.MyCommand"
+    Write-Host "Введите хотя бы одно слово"
     exit 1
 }
 
-# Check if the second argument is provided
-if ($args[1]) {
-    Write-Output "Extra parameters found"
-    exit 1
-}
-
-# Check if the dictionary file exists
-if (-not (Test-Path "bd.txt")) {
-    Write-Output "Error: the dictionary file does not exist"
-    exit 1
-}
-
-# Search for the word in the dictionary file
-$OUT = Select-String -Pattern $args[0] -Path "bd.txt" | Select-Object -First 1 | ForEach-Object { $_.Line -replace "$args[0] " }
-
-# If the word is found in the dictionary
-if ($OUT) {
-    Write-Output "This is: $OUT"
-    exit 0
-}
-else {
-    $yn = Read-Host -Prompt "Word wasn't found, wanna add it to the dictionary? [y/n]"
-    switch ($yn.ToLower()) {
-        'y' {
-            Write-Output ""
-            break
-        }
-        'n' {
-            Write-Output ""
-            exit 0
-        }
-        default {
-            Write-Output ""
-            exit 1
-        }
+foreach ($var in $args) {
+    if ($var -like "*ая*" -or $var -like "*яя*" -or $var -like "*ой*" -or $var -like "*ей*" -or $var -like "*ую*" -or $var -like "*юю*" -or $var -like "*ой*") {
+        Write-Host "$var - adjective."
     }
-    
-    $partOfSpeech = Read-Host -Prompt "Enter part of speech for this word"
-    $result = "$($args[0]) $($partOfSpeech)"
-    Add-Content -Path "bd.txt" -Value $result
-    exit 0
+    elseif ($var -like "*ый*" -or $var -like "*ий*" -or $var -like "*ого*" -or $var -like "*его*" -or $var -like "*ому*" -or $var -like "*ему*" -or $var -like "*ым*") {
+        Write-Host "$var - adjective."
+    }
+    elseif ($var -like "*им*" -or $var -like "*ом*" -or $var -like "*ем*" -or $var -like "*ое*" -or $var -like "*ее*" -or $var -like "*ые*" -or $var -like "*ие*") {
+        Write-Host "$var - adjective."
+    }
+    elseif ($var -like "*их*" -or $var -like "*ых*" -or $var -like "*ым*" -or $var -like "*им*" -or $var -like "*ых*" -or $var -like "*их*" -or $var -like "*ыми*" -or $var -like "*ими*") {
+        Write-Host "$var - adjective."
+    }
+    elseif ($var -like "*у*" -or $var -like "*ю*" -or $var -like "*ешь*" -or $var -like "*ем*" -or $var -like "*ете*" -or $var -like "*ет*" -or $var -like "*ут*" -or $var -like "*ют*" -or $var -like "*ть*") {
+        Write-Host "$var - verb."
+    }
+    elseif ($var -like "*им*" -or $var -like "*ишь*" -or $var -like "*ите*" -or $var -like "*и*" -or $var -like "*ат*" -or $var -like "*ят*" -or $var -like "*еть*" -or $var -like "*ить*" -or $var -like "*ать*" -or $var -like "*ла*" -or $var -like "*ло*" -or $var -like "*ли*" -or $var -like "*ил*" -or $var -like "*ел*") {
+        Write-Host "$var - verb."
+    }
+    elseif ($var -like "*а*" -or $var -like "*я*" -or $var -like "*о*" -or $var -like "*е*" -or $var -like "*ы*" -or $var -like "*и*" -or $var -like "*и*" -or $var -like "*е*" -or $var -like "*б*") {
+        Write-Host "$var - noun."
+    }
+    else {
+        Write-Host "$var - nonparseble."
+    }
 }

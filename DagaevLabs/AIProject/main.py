e653e194f7e9546b4e4599e603d9505ec9ec467c
wemik2323 <wemik232@yandex.ru>
2023-12-27 03:27:10 +0300

    AIProject for DagaevLabs added

---
+++ DagaevLabs/AIProject/main.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+
+import cv2
+import numpy as np
+
+def imgParse(image_path):
+
+    # Загрузка и предобработка изображения
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+    if img is None:
+        print(f"Ошибка загрузки изображения {image_path}")
+        return
+    
+    img = cv2.resize(img, (28, 28))
+    img = img.reshape((1, 28, 28, 1)).astype('float32') / 255
+
+    # Предсказание
+    predictions = model.predict(img)
+    predicted_label = np.argmax(predictions[0])
+
+    # Вывод предсказания
+    print(f"Модель предсказывает: {predicted_label}")
+
+# Загрузка данных MNIST
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Предобработка данных
+train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+
+# Кодирование меток в формат one-hot
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+# Создание модели
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Компиляция модели
+model.compile(optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
+        
+# Обучение модели
+model.fit(train_images, train_labels, epochs=100, batch_size=64, validation_split=0.2)
+
+# Оценка модели на тестовых данных
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f"Точность на тестовом наборе: {test_acc}")
+
+imgParse('test.jpg')
+imgParse('test2.jpg')
+imgParse('test3.jpg')
+imgParse('test4.jpg')
+imgParse('test5.jpg')
+imgParse('test6.jpg')
+

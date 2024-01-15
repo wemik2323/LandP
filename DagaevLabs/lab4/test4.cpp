206ca84b2c8d4e8d8bce20a577a13e4413783af0
wemik2323 <wemik232@yandex.ru>
2024-01-15 07:16:48 +0300

    DagaevLabs reports fixed (AI report not done yet)

--- DagaevLabs/lab4/test4.cpp
+++ DagaevLabs/lab4/test4.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <mutex>
 #include <random>
 #include <chrono>
 #include <string>
 #include <windows.h>
 
-std::mutex mtx;
+HANDLE mtx;
 bool programStop = false;
 int secretNumber = 0;
 
@@ -19,12 +18,14 @@ DWORD WINAPI generateSecretNumber(LPVOID lpParam) {
     std::uniform_int_distribution<> distr(1, 100);
 
     while (!programStop) {
-        std::unique_lock<std::mutex> lock(mtx);
+        WaitForSingleObject(mtx, INFINITE);
+
         secretNumber = distr(gen);
         std::cout << "Thread 1: New secret number is generated: " << secretNumber << std::endl;
         numbersGenerated++;
-        lock.unlock();
-        Sleep(2000);  // Используем Sleep вместо std::this_thread::sleep_for
+
+        ReleaseMutex(mtx);
+        Sleep(2000);
     }
 
     return 0;
@@ -36,9 +37,10 @@ DWORD WINAPI guessNumber(LPVOID lpParam) {
     std::uniform_int_distribution<> distr(1, 100);
 
     while (!programStop) {
-        Sleep(500);  // Используем Sleep вместо std::this_thread::sleep_for
+        Sleep(500);
 
-        std::unique_lock<std::mutex> lock(mtx);
+        WaitForSingleObject(mtx, INFINITE);
+
         int guess = distr(gen);
         std::cout << "Thread 2: Guessing " << guess << std::endl;
         numberGuessAttempts++;
@@ -47,6 +49,8 @@ DWORD WINAPI guessNumber(LPVOID lpParam) {
             std::cout << "Thread 2: Guessed the number " << secretNumber << std::endl;
             numbersGuessed++;
         }
+
+        ReleaseMutex(mtx);
     }
 
     return 0;
@@ -57,8 +61,11 @@ DWORD WINAPI userInput(LPVOID lpParam) {
     while (true) {
         std::getline(std::cin, input);
         if (input == "s") {
-            std::unique_lock<std::mutex> lock(mtx);
+            WaitForSingleObject(mtx, INFINITE);
+
             programStop = true;
+
+            ReleaseMutex(mtx);
             break;
         }
     }
@@ -67,6 +74,8 @@ DWORD WINAPI userInput(LPVOID lpParam) {
 }
 
 int main() {
+    mtx = CreateMutex(NULL, FALSE, NULL);
+
     HANDLE threads[3];
     threads[0] = CreateThread(NULL, 0, generateSecretNumber, NULL, 0, NULL);
     threads[1] = CreateThread(NULL, 0, guessNumber, NULL, 0, NULL);
@@ -78,13 +87,15 @@ int main() {
     CloseHandle(threads[1]);
     CloseHandle(threads[2]);
 
+    CloseHandle(mtx);
+
     std::cout << "Program finished!" << std::endl;
     std::cout << "Number of times the number was generated: " << numbersGenerated << std::endl;
     std::cout << "Number of times the number was guessed: " << numbersGuessed << std::endl;
     std::cout << "Number of times the number was attempted to guess: " << numberGuessAttempts << std::endl;
     std::cout << std::endl << "Statistics: " << std::endl;
-    std::cout << "Success attempts: " << numbersGuessed/numberGuessAttempts << "freq" << " or " << (numbersGuessed/numberGuessAttempts)*100 << "%" << std::endl;
-    std::cout << "Failure attempts: " << (numberGuessAttempts-numbersGuessed)/numberGuessAttempts << "freq" << " or " << ((numberGuessAttempts-numbersGuessed)/numberGuessAttempts)*100 << "%" << std::endl;
-    
+    std::cout << "Success attempts: " << numbersGuessed / numberGuessAttempts << "freq" << " or " << (numbersGuessed / numberGuessAttempts) * 100 << "%" << std::endl;
+    std::cout << "Failure attempts: " << (numberGuessAttempts - numbersGuessed) / numberGuessAttempts << "freq" << " or " << ((numberGuessAttempts - numbersGuessed) / numberGuessAttempts) * 100 << "%" << std::endl;
+
     return 0;
 }
